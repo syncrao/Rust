08f378b8e5e1f3ea396ce9b58b3834f3cfd39431
raoshah <shahrukh1@gmail.com>
2024-05-26 15:56:17 +0530

    test

--- chapter2/src/main.rs
+++ chapter2/src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
     loop {
-        println!("Please input your guess ");
+        println!("Please input your guess test ");
         let mut guess = String::new();
         std::io::stdin().read_line(&mut guess).expect("Failed to read line");
         let guess: u32 = match guess.trim().parse() { Ok(num) => num, Err(_) => continue, };
