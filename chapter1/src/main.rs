2e1830a3b4b56a43730102e19bc628e8a074896c
raoshah <shahrukh1@gmail.com>
2024-07-13 20:14:43 +0530

    13 july

--- chapter1/src/main.rs
+++ chapter1/src/main.rs
@@ -1,9 +1,19 @@
 fn main() {
     println!("Hello, world!");
-    chapter1()
+    chapter1();
+    july13()
 }
 
 
 fn chapter1() {
     println!("chapter complate");
 }
+
+fn july13() {
+    let mut x = 5;
+    println!("{x}");
+   
+    println!("{Y}");
+}
+
+const Y: i32 = 1000;
