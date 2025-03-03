ba8fff1af1aaf57bc7e7439ce2cb5a49dedc1bd2
raoshah <shahrukhrao1@gmail.com>
2025-03-03 20:34:55 +0530

    May2025

--- test1/src/main.rs
+++ test1/src/main.rs
@@ -1,17 +1,8 @@
-
-fn main(){
-
-    let x: u32 = 2;
-    let y: u32 = 2;
-
-    match x {
-       y => println!("yes")
-       `y => println!("No")
-    };
-    let z  = x.cmp(&y);
-    match x {
-        y => println!("yes")
-        `z => println!("No")
-     };
-    println!("{:?}", z)
+use std::thread;
+fn main() {
+let list = vec![1, 2, 3];
+println!("Before defining closure: {list:?}");
+thread::spawn(|| println!("From thread: {list:?}"))
+.join()
+.unwrap();
 }
