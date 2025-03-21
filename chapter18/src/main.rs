fc34d67e898305f4501e0c2163228608be2317b0
raoshah <shahrukhrao1@gmail.com>
2025-03-21 22:16:46 +0530

    Mar21

--- chapter18/src/main.rs
+++ chapter18/src/main.rs
@@ -16,4 +16,61 @@ fn main() {
     } else {
         println!("Using blue as the background color");
     }
+
+    let mut stack = Vec::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    while let Some(top) = stack.pop() {
+        println!{"{top}"}
+    }
+    
+    let v = vec!['a', 'b', 'c'];
+
+    for (index, value) in v.iter().enumerate() {
+        println!("{value} is at index {index}");
+    }
+
+    let x = Some(5); 
+    let y = 10; 
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _=> println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}");
+
+    let x = 5;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        1..=5 => println!("three"),
+        _=> println!("Other"),
+    }
+
+    let x = Some(5);
+
+    match x {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number {x} is odd"),
+        None => (), 
+    }
+
+    enum Message {
+        Hello {id: i32},
+    }
+
+    let msg = Message::Hello{id:101};
+
+    match msg {
+        Message::Hello {id: id_variable @ 3..=7,} => println!("Found an id in range: {id_variable}"),
+        Message::Hello {id: 10..=12} => println!("Found an id in another range"),
+        Message::Hello {id} => println!("Found some other id: {id}"),
+    }
+
+
 }
