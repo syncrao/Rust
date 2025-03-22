7a4021061fac2295162253f70fef7c1fe7232aba
raoshah <shahrukhrao1@gmail.com>
2025-03-22 18:11:08 +0530

    Mar22

---
+++ chapter20/src/main.rs
@@ -0,0 +1,41 @@
+use std::{
+    fs,
+    net::{TcpListener, TcpStream},
+    io::{prelude::*, BufReader}
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        
+        handle_connection(stream);
+
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    println!("{request_line:?}");
+
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
+    
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+            );
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let contents = fs::read_to_string("404.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent_Length: {length}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+}
